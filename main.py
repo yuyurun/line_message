58b2d446101f2c09161b92510d1c0a1a06125693
yuyurun <yuyu1.run@icloud.com>
2019-10-19 20:32:25 +0900

    portの設定戻す

--- main.py
+++ main.py
@@ -46,6 +46,6 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run()
-#    port = int(os.getenv("PORT", 5000))
-#    app.run(host="0.0.0.0", port=port)
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

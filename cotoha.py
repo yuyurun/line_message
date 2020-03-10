1b9afcb1b6eba25641ef060a5ef941ede3d36226
yuyurun <yuyu1.run@icloud.com>
2020-03-10 16:47:43 +0900

    fix reply

--- cotoha.py
+++ cotoha.py
@@ -66,7 +66,7 @@ def convert(r_parse, r_type):
                 if token['pos'] == '動詞語幹':
                     f = token['lemma']
                     print(f)
-                elif token['form'] == 'う' and token['form'] == '動詞接尾辞' and len(f) > 0:
+                elif token['form'] == 'う' and token['pos'] == '動詞接尾辞' and len(f) > 0:
                     response = f + '!!'
     return response
 
